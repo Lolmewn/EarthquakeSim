532dc1d9f270521b0a561765f3674498fce3f3e3
Sybren Gjaltema <lolmewn@gmail.com>
2016-03-08 18:23:27 +0100

    Spark it up

--- QuakeMonitor/src/main/java/nl/lolmewn/rug/quakemonitor/rest/Status.java
+++ QuakeMonitor/src/main/java/nl/lolmewn/rug/quakemonitor/rest/Status.java
@@ -1,27 +1,18 @@
 package nl.lolmewn.rug.quakemonitor.rest;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 import nl.lolmewn.rug.quakemonitor.net.Server;
+import static spark.Spark.*;
 
 /**
  * @author Sybren
  */
-@Path("status")
 public class Status {
 
     private final Server server;
 
     public Status(Server server) {
         this.server = server;
-    }
-
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String getPort() {
-        return Integer.toString(server.getPort());
+        get("/status", (req, res)-> "");
     }
 
 }
