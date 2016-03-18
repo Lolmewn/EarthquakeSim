f0abe8646c59ce22392d127eee513ff5b8634a5f
Sybren Gjaltema <lolmewn@gmail.com>
2016-03-18 12:12:03 +0100

    Finish the QuakeGraph

--- QuakeSensor/src/main/java/nl/lolmewn/rug/quakesensor/gui/QuakeGraph.java
+++ QuakeSensor/src/main/java/nl/lolmewn/rug/quakesensor/gui/QuakeGraph.java
@@ -14,24 +14,26 @@ public class QuakeGraph extends JPanel {
     /**
      * Every 5 pixels is one data point
      */
-    private static final CircularFifoQueue<SenseData> QUEUE = new CircularFifoQueue(216); // 1080 pixels worth of data should be enough
+    private static final int PIXELS_PER_TICK = 2;
+    private static final CircularFifoQueue<SenseData> QUEUE = new CircularFifoQueue(1080 / QuakeGraph.PIXELS_PER_TICK); // 1080 pixels worth of data should be enough
 
     @Override
     protected void paintComponent(Graphics grphcs) {
         super.paintComponent(grphcs);
-        int startX = this.getWidth() - QUEUE.size(); // old entries will be drawn outside of the screen, and that's okay
+        int startX = this.getWidth() - QUEUE.size() * PIXELS_PER_TICK; // old entries will be drawn outside of the screen, and that's okay
 
-        int thisNeedsABetterName = this.getHeight() / 2;
+        int margin = this.getHeight() / 2; // Don't know a good name for this variable
         int middle = this.getHeight() / 2;
         int startY = middle;
         synchronized (QUEUE) {
-            QUEUE.forEach((data) -> {
+            for (SenseData data : QUEUE) {
                 // do something with the data
-                int endY = (int) (data.getGroundAcceleration() * thisNeedsABetterName + middle);
-                grphcs.drawLine(startX, startY, startX + 5, endY);
-            });
+                int endY = (int) (data.getGroundAcceleration() * margin + middle);
+                grphcs.drawLine(startX, startY, startX + PIXELS_PER_TICK, endY);
+                startX += PIXELS_PER_TICK;
+                startY = endY;
+            }
         }
-        grphcs.drawLine(5, 5, 50, 50);
     }
 
     public static void addDatapoint(SenseData data) {
